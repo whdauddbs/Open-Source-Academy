e60121a997567436796c7cc75692349a80f8bee4
whdauddbs <whdauddbss@naver.com>
2017-10-17 19:34:03 +0900

    complete Permission

--- Pasugoon/app/src/main/java/com/example/administrator/pasugoon/MainActivity.java
+++ Pasugoon/app/src/main/java/com/example/administrator/pasugoon/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.administrator.pasugoon.R;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
